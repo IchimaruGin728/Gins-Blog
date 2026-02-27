f3577f454e046de58adf4c62f50f6e90422bfb7b
IchimaruGin <ichimarugin728@gmail.com>
2026-02-27 14:29:48 +0800

    fix(auth): fix broken OS detection logic and refine touch detection threshold

--- src/pages/api/sessions.ts
+++ src/pages/api/sessions.ts
@@ -48,6 +48,11 @@ export const GET: APIRoute = async ({ locals, request, cookies }) => {
 		else if (ua.includes("Firefox")) browser = "Firefox";
 		else if (ua.includes("Edge")) browser = "Edge";
 
+		if (ua.includes("Windows")) os = "Windows";
+		else if (ua.includes("Mac")) os = "macOS";
+		else if (ua.includes("Linux")) os = "Linux";
+		else if (ua.includes("Android")) os = "Android";
+		else if (ua.includes("iOS") || ua.includes("iPhone") || ua.includes("iPad"))
 			os = "iOS";
 		
 		if (session.osVerified) {
